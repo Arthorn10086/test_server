f9b270449e5bab90b4017217269bc166181a165b
user <674646973@qq.com>
2020-01-19 16:20:47 +0800

    arthorn

--- src/server_db/server_db_file.erl
+++ src/server_db/server_db_file.erl
@@ -14,10 +14,10 @@
     terminate/2,
     code_change/3]).
 
--export([lock/5, update/5, delete/5, get/5]).
+-export([lock/5, update/5, delete/5, get/5, batch_write/5]).
 
 -define(SERVER, ?MODULE).
--define(INTERVAL, 1000).
+-define(CAHCE_INTERVAL, 30000).
 -include("../../include/server.hrl").
 
 -record(state, {db_name, key, interval, cahce_tactics, cahce_size, cahce_time, key_ets, ets, fields, cahce_ref, write_ref}).
@@ -44,14 +44,16 @@ init({ID, Name, Options}) ->
     %%拿到所有字段
     FieldsSql = server_db_lib:get_fields_sql(Name),
     {_, _, Fields} = mysql_poolboy:query(?POOLNAME, FieldsSql),
-    FieldL = lists:reverse([list_to_atom(binary:bin_to_list(B)) || B <- lists:flatten(Fields)]),
+    FieldL = [list_to_atom(binary:bin_to_list(B)) || B <- lists:flatten(Fields)],
     case FieldL of
         [] ->%%表不存在
             {stop, 'none_table'};
         FieldL ->
-            {_, InterVal} = lists:keyfind('interval', 1, Options),
+            {_, InterVal} = lists:keyfind('interval', 1, Options),%秒
             {_, KeyName, KeyType} = lists:keyfind('key', 1, Options),
             {_, Tactics} = lists:keyfind('cache_tactics', 1, Options),
+            {_, CacheTime} = lists:keyfind('cache_time', 1, Options),
+            {_, CachaSize} = lists:keyfind('cache_size', 1, Options),%KB
             Ets = ets:new(?MODULE, ['protected', 'set']),
             KeyEts = ets:new(?MODULE, ['protected', 'ordered_set']),
             %%加载所有Key
@@ -67,13 +69,15 @@ init({ID, Name, Options}) ->
                     DayTime = Now-ZeroTime,
                     NextTime = trunc(DayTime / InterVal) * InterVal + InterVal,
                     DifTime = NextTime-DayTime,
-                    erlang:start_timer(DifTime, self(), 'batch_write');
+                    erlang:start_timer(DifTime * 1000, self(), 'batch_write');
                 true ->
                     ok
             end,
+            CahceRef = erlang:start_timer(?CAHCE_INTERVAL, self(), 'refresh_cahce'),
+            SizeOut = CachaSize * 1024 div erlang:system_info(wordsize),
             gen_server:enter_loop(?MODULE, [],
                 #state{db_name = Name, interval = InterVal, key = {KeyName, KeyType}, ets = Ets, key_ets = KeyEts,
-                    fields = FieldL, cahce_tactics = Tactics, write_ref = Ref}, {local, ID})
+                    fields = FieldL, cahce_time = CacheTime, cahce_size = SizeOut, cahce_tactics = Tactics, write_ref = Ref, cahce_ref = CahceRef}, {local, ID})
     end.
 
 
@@ -83,20 +87,29 @@ handle_call({Action, Key, Value, Vsn, LockTime, Locker}, _From, State) ->
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
-
+handle_cast({batch_write}, #state{db_name = DBName, ets = Ets, fields = Fields, key = {KeyName, KeyType}} = State) ->
+    batch_write(DBName, Ets, Fields, KeyName, KeyType),
+    io:format("batch_write_ok =~p~n", [DBName]),
+    {noreply, State};
 handle_cast(_Request, State) ->
     {noreply, State}.
 
-
-handle_info({time_out, Ref, 'batch_write'}, #state{db_name = DBName, ets = Ets, key = {KeyName, KeyType}, cahce_tactics = Tactics, interval = InterVal, fields = Fields, write_ref = Ref} = State) ->
+%%持久化
+handle_info({timeout, Ref, 'batch_write'}, #state{db_name = DBName, ets = Ets, key = {KeyName, KeyType}, cahce_tactics = Tactics, interval = InterVal, fields = Fields, write_ref = Ref} = State) ->
     case Tactics of
         write_behind ->
-            erlang:start_timer(InterVal, self(), 'batch_write'),
-            batch_write(DBName, Ets, Fields, KeyName, KeyType);
+            NRef = erlang:start_timer(InterVal * 1000, self(), 'batch_write'),
+            batch_write(DBName, Ets, Fields, KeyName, KeyType),
+            {noreply, State#state{write_ref = NRef}};
         write_through ->
-            ok
-    end,
-    {noreply, State};
+            {noreply, State}
+    end;
+%%刷新缓存
+handle_info({timeout, Ref, 'refresh_cahce'}, #state{cahce_ref = Ref} = State) ->
+    MS = time_lib:now_millisecond(),
+    NewRef = erlang:start_timer(?CAHCE_INTERVAL, self(), 'refresh_cahce'),
+    remove_cold_data(State, MS),
+    {noreply, State#state{cahce_ref = NewRef}};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -159,9 +172,8 @@ get(State, Key, _, _, MS) ->
                             ets:insert(Ets, {Key, Maps, MS, 0}),
                             {ok, Maps, 0, MS}
                     end;
-                [{Key, 'delete'}] ->
-                    none;
                 [{Key, Maps, Time, Version}] ->
+                    ets:update_element(Ets, Key, {3, MS}),
                     {ok, Maps, Version, Time}
             end
     end.
@@ -196,7 +208,8 @@ delete(State, Key, _, _, _) ->
     case Tactics of
         write_behind ->
             ets:delete(KeyEts, Key),
-            ets:insert(Ets, {Key, 'delete'});
+            ets:insert(Ets, {Key, 'delete', 0, 1}),
+            ok;
         write_through ->
             delete_mysql_value(State, Key),
             ets:delete(KeyEts, Key),
@@ -278,14 +291,14 @@ batch_write(DBName, Ets, Fields, KeyName, KeyType) ->
     {ok, I2} = mysql:prepare(Conn, DelPrepare),
     F = fun({_, _, _, 0}, _Acc) ->
         ok;
-        ({Key, Maps, Time, _Version}, _Acc) ->
+        ({Key, 'delete', 0, 1}, _Acc) ->
+            mysql:execute(Conn, I2, [Key]),
+            ets:delete(Ets, Key),
+            ok;
+        ({Key, Maps, _Time, _Version}, _Acc) ->
             Params = to_params(Fields, maps:to_list(Maps)),
             mysql:execute(Conn, I1, Params),
-            ets:insert(Ets, {Key, Maps, Time, 0}),
-            ok;
-        ({Key, 'delete'}, _Acc) ->
-            mysql:execute(Conn, I2, Key),
-            ets:delete(Ets, Key),
+            ets:update_element(Ets, Key, {4, 0}),
             ok
     end,
     ets:foldl(F, [], Ets),
@@ -295,3 +308,87 @@ batch_write(DBName, Ets, Fields, KeyName, KeyType) ->
     ok.
 
 
+remove_cold_data(State, MS) ->
+    #state{ets = Ets, cahce_tactics = Tactics} = State,
+    case is_behind(Tactics) of
+        true ->%清理缓存的时候要同步数据库
+            remove_time_out1(State, MS),
+            remove_overflow(State, MS);
+        false ->
+            %%过期数据
+            remove_time_out(Ets, MS),
+            %%溢出数据
+            remove_overflow(State, MS)
+
+    end.
+
+
+%% 10W数据筛选删除5W条数据   select_delete 12毫秒    ets:foldl 300毫秒      select再lists:foreach删除 80毫秒
+
+%%如果刷硬盘的间隔时间小于缓存过期时间直接删除冷数据
+remove_time_out(Ets, MS) ->
+    ets:select_delete(Ets, [{{'$1', '$2', '$3', '$4'}, [{'=<', '$3', MS}], [true]}]).
+
+%%如果刷硬盘的间隔时间大于缓存过期时间,清理时要刷到数据库
+remove_time_out1(State, MS) ->
+    #state{db_name = DBName, key = {KeyName, KeyType}, fields = Fields, ets = Ets} = State,
+    UpdateL = ets:select(Ets, [{{'$1', '$2', '$3', '$4'}, [{'=<', '$3', MS}, {'>', '$4', 0}], [{{'$1', '$2'}}]}]),
+    remove_time_out(Ets, MS),
+    ReplacePrepare = get_replace_prepare(DBName, Fields),
+    DelPrepare = get_delete_prepare(DBName, KeyName, KeyType),
+    Conn = poolboy:checkout(?POOLNAME2),
+    {ok, I1} = mysql:prepare(Conn, ReplacePrepare),
+    {ok, I2} = mysql:prepare(Conn, DelPrepare),
+    lists:foreach(fun({Key, 'delete'}) ->
+        mysql:execute(Conn, I2, [Key]);
+        ({_, Maps}) ->
+            Params = to_params(Fields, maps:to_list(Maps)),
+            mysql:execute(Conn, I1, Params)
+    end, UpdateL),
+    mysql:unprepare(Conn, I1),
+    mysql:unprepare(Conn, I2),
+    poolboy:checkin(?POOLNAME2, Conn),
+    ok.
+
+%%溢出
+remove_overflow(State, MS) ->
+    #state{ets = Ets, cahce_size = SizeOut, cahce_time = TimeOut, cahce_tactics = Tactics} = State,
+    Info = ets:info(Ets),
+    {_, Size} = lists:keyfind(size, 1, Info),
+    {_, Memory} = lists:keyfind(memory, 1, Info),
+    %估算预计多少条数据缓存上限
+    Size1 = trunc(SizeOut / Memory * SizeOut),
+    %%保留上限的2/3
+    RetainSize = Size1 * 2 div 3,
+    Range = TimeOut div 10,
+    if
+        Memory > SizeOut andalso Size > 0 ->
+            %%获得所有数据时间分布
+            TimeRange = ets:foldl(fun({_, _, _, T}, R) ->
+                I = (MS - T) div Range + 1,
+                erlang:setelement(I, R, erlang:element(I, R) + 1)
+            end, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, Ets),
+            %%计算删除的时间点
+            Time = get_remove_time(TimeRange, Range, RetainSize, tuple_size(TimeRange), 0, 0),
+            case is_behind(Tactics) of
+                true ->
+                    remove_time_out1(State, MS - Time);
+                false ->
+                    remove_time_out(Ets, MS - Time)
+            end,
+            ok;
+        true ->
+            ok
+    end.
+
+%计算开始移除的时间段
+get_remove_time(_TimeRange, _Range, _RetainSize, 0, _OldCount, _NewCount) ->
+    0;
+get_remove_time(_TimeRange, Range, RetainSize, I, _OldCount, NewCount) when NewCount == RetainSize ->
+    Range * I;
+get_remove_time(_TimeRange, Range, RetainSize, I, OldCount, NewCount) when NewCount > RetainSize ->
+    Time1 = Range * I,
+    Time1 + trunc((1 - (RetainSize - OldCount) / (NewCount - OldCount)) * Range);
+get_remove_time(TimeRange, Range, RetainSize, I, _OldCount, NewCount) ->
+    Add = element(I, TimeRange),
+    get_remove_time(TimeRange, Range, RetainSize, I - 1, NewCount, NewCount + Add).
